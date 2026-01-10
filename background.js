1b8b1b6999f18807091447da7fcd21c0386d80e9
AlexBun9 <129278726+AlexBun9@users.noreply.github.com>
2026-01-10 19:55:47 +0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+﻿//поиск элемента и закрытие с интервалами
+chrome.runtime.onMessage.addListener((msg, sender) => {
+  if (msg.action === 'closeTab' && sender.tab) {
+    setTimeout(() => chrome.tabs.remove(sender.tab.id), 2000);
+  }
+  return true;
+});
---
+++ closepage.js
@@ -0,0 +1,7 @@
+﻿//поиск элемента и закрытие с интервалами
+setInterval(() => {
+  const found = document.querySelector('.b24-form-state-text');
+  if (found && found.textContent.includes('Спасибо, Ваше обращение принято')) {
+    chrome.runtime.sendMessage({ action: 'closeTab' });
+  }
+}, 5000);
